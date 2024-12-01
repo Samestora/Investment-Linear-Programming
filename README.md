f07a7b0557a993e089b80fed1bdd5cbd8c387afd
Samestora <putrantosurya38@gmail.com>
2024-12-01 23:14:38 +0700

    FIX

--- README.md
+++ README.md
@@ -1,5 +1,20 @@
 # Investment Planner Using Linear Programming
 Involving 3 Variables :  
-Stocks, Obligation, Real Estate (Property)
+- X = Saham
+- Y = Obligasi
+- Z = Real Estate (Property)
+
+Seluruh investasi harus sesuai budget,  
+> x + y + z <= Total Pool
+
+Maksimum investasi di saham,  
+> x + 0y + 0z <= Pool * 50%
+
+Minimum investasi di obligasi,
+> 0x + 1y + 0z >= Pool * 25%
+
+Minimum investasi di properti,  
+> 0x + 0y + z >= Pool * 35% 
+
 ### Developing
 ``` pip install -r requirements.txt ```
--- main.py
+++ main.py
@@ -5,38 +5,44 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 # Koefisien untuk fungsi objektif (negatif karena kita memaksimalkan keuntungan)
-c = [-0.1, -0.05, -0.07]
+c = [-0.1, -0.05, -0.07]  # -P(x,y,z) = - 0.1x - 0.05y - 0.07z
 
 # Matriks koefisien untuk batasan
-A = [[1, 1, 1],    # Total investasi
-     [-1, 0, 0],   # Minimum investasi di saham
-     [0, 0, 1]]    # Maksimum investasi di properti
-b = [150000, -60000, 45000]
-
-# Batasan untuk keuntungan tahunan minimal
-A_eq = [[0.1, 0.05, 0.07]]
-b_eq = [10000]
+A = [[1, 1, 1],    # Total investasi -> x + y + z <= 150000
+     [1, 0, 0],  # Maksimum investasi di saham -> x + 0y + 0z <= 60000 -> x <= 60000
+     [0, -1, 0], # Minimum di obligasi
+     [0, 0, -1]]    # Minimum investasi di properti -> 0x + 0y + -z >= -45000
 
 # Batas untuk variabel investasi (harus non-negatif)
 bounds = [(0, None), (0, None), (0, None)]
 
-result = linprog(c, A_ub=A, b_ub=b, A_eq=A_eq, b_eq=b_eq, bounds=bounds, method="highs")
-
-# Hasil
-if result.success:
-    investment_in_shares, investment_in_bonds, investment_in_property = result.x
-    print(f"Jumlah investasi di saham: ${investment_in_shares:.2f}")
-    print(f"Jumlah investasi di obligasi: ${investment_in_bonds:.2f}")
-    print(f"Jumlah investasi di properti: ${investment_in_property:.2f}")
-else:
-    print("Solusi tidak ditemukan.")
-
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html',
-                           investment_in_shares=investment_in_shares,
-                           investment_in_bonds=investment_in_bonds,
-                           investment_in_property=investment_in_property
-                           )
+    if request.method == 'POST':
+        # Ambil nilai dari form input
+        b_values = [
+            float(request.form['pool']),
+            float(0.5 * float(request.form['pool'])),
+            float(0.25 * -1 *float(request.form['pool'])),
+            float(0.35 * -1 *float(request.form['pool']))
+            ]
+        
+        # Hitung hasil linear programming dengan data input baru
+        result = linprog(c, A_ub=A, b_ub=b_values, bounds=bounds, method="highs")
+        
+        if result.success:
+            investment_in_shares, investment_in_bonds, investment_in_property = result.x
+            profit = -result.fun
+            return render_template('index.html',
+                                   investment_in_shares=investment_in_shares,
+                                   investment_in_bonds=investment_in_bonds,
+                                   investment_in_property=investment_in_property,
+                                   profit=profit)
+        else:
+            return "Solusi tidak ditemukan."
+
+    return render_template('index.html', investment_in_shares=None, investment_in_bonds=None, investment_in_property=None, profit=None)
+
 if __name__ == '__main__':
     app.run(debug=True)
+
